f8036592688cc1fd0a0ba0959f07052c0e45c9bb
Zex1l <kriborukov2007@mail.ru>
2022-08-09 19:41:34 +0300

    Added home page with top musics chart, added musics service

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,58 @@
-
+import { useEffect, useState } from "react";
+import MusicService from "../Service/MusicServices";
 import "../styles/style.css"
 
 const HomePage = () => {
+    const [topChart, setTopChart] = useState([])
+    const {getTopMusics, loading} = MusicService()
+
+    useEffect(() => {
+        getTopMusics()
+        .then(responce => setTopChart(responce))
+    }, [])
+
+    
+    const renderTopChart = (topChart) => {
+        const items = topChart.map(chart => {
+            return (
+                <div className="topChart__item">
+                    <img className="topChart__img" src={chart.img} alt="" />
+                    <div className="topChart__music">{chart.title}</div>
+                    <div className="topChart__name">{chart.autor}</div>
+                    <a href={chart.urlMusic} className="topChart__link">Go to Shazam</a>
+                </div>
+            )
+        })
+        return items
+    }
+    
+    const content = renderTopChart(topChart)
+
     return (
         <div className="main">
             <div className="container">
-                Main
+                <div className="main__inner">
+                    <h1>WELCOME TO MUSIC REACT APP</h1>
+                    <h2>Here you will find your favorite music</h2>
+                    
+                    <div className="topChart">
+                        <div className="topChart__logo">Top char musics</div>
+                        <div className="topChart__grid">
+                            {content}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
+
+const View = (chart) => {
+    return (
+        <div className="topCHar__item">
+            <img className="topChart__img" src={chart.img} alt="" />
+        </div>
+    )
+}
 
 export default HomePage;
--- src/Service/MusicServices.js
+++ src/Service/MusicServices.js
@@ -1,3 +1,32 @@
-class MesicServies {
-    
+import { useHttp } from "../Hooks/useHttp"
+
+const MusicService = () => {
+    const {request, loading} = useHttp()
+
+    const _topMusicUrl = "https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0"
+    const options= {
+        headersTopMusic : {
+            'X-RapidAPI-Key': '16a0768e11msh4ff883b744d5c9ap1dfa93jsn162358fb5542',
+		    'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        }
+    }
+
+    const getTopMusics = async() => {
+        const data = await request(_topMusicUrl, "GET", null, options.headersTopMusic)
+        
+        return data.tracks.map(_transformMusic)
+    }
+
+
+    const _transformMusic = (music) => {
+        return {
+            title: music.title,
+            autor: music.subtitle,
+            urlMusic: music.url,
+            img: music.share.image
+        }
+    }
+
+    return {getTopMusics, loading}
 }
+export default MusicService
